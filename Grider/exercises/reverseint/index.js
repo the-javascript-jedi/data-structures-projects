dc960e1c8fb38ec2802f9b525de8f69c94a28126
the-javascript-jedi <nithin.thewebdev@gmail.com>
2023-07-25 22:22:55 +0530

    configured test and added integer reverse

--- Grider/exercises/reverseint/index.js
+++ Grider/exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
-
-module.exports = reverseInt;
+function reverseInt(n) {
+    var reversed = n.toString().split("").reverse().join("");
+    // we will use Math.sign() to include the sign of the number i.e)-15==-51
+    // The Math.sign() static method returns 1 or -1, indicating the sign of the number passed as argument. If the input is 0 or -0, it will be returned as-is.   
+    return parseInt(reversed)*Math.sign(n);
+ }
+ module.exports = reverseInt;
+ 
