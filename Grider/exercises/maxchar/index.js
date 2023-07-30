0f6badad662df1e431a32ad0f7170281b56b18d6
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-07-30 20:03:29 +0530

    added maxchars solution

--- Grider/exercises/maxchar/index.js
+++ Grider/exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+  // for of used to iterate an array
+  for (let char of str) {
+    // if object key does not exist create the object key, if key exists increment the key
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  // for in used to iterate an object
+  for (let char in charMap) {
+    //   maxChar("abcccccccd") === "c"
+    //   {
+    //     max:maxChar // c:7
+    //   }
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  console.log("charMap", charMap);
+  console.log("max", max);
+  console.log("maxChar", maxChar);
+  return maxChar;
+}
 
 module.exports = maxChar;
