9ceda5609f3e2d258bb21b0fb1c844029cb2ae53
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-06 21:35:51 +0530

    added iterative solution

--- Grider/exercises/steps/index.js
+++ Grider/exercises/steps/index.js
@@ -1,4 +1,6 @@
 // --- Directions
+// jest steps/test.js --watch
+//
 // Write a function that accepts a positive number N.
 // The function should console log a step shape
 // with N levels using the # character.  Make sure the
@@ -17,6 +19,18 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
 
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
 module.exports = steps;
