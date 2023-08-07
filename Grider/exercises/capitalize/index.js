2b23214953ef454a240c15792a3a2810d4a5874a
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-07 21:42:18 +0530

    added solution 1

--- Grider/exercises/capitalize/index.js
+++ Grider/exercises/capitalize/index.js
@@ -1,4 +1,6 @@
 // --- Directions
+// to run =>  jest capitalize/test.js --watch
+//////////////////////////////
 // Write a function that accepts a string.  The function should
 // capitalize the first letter of each word in the string then
 // return the capitalized string.
@@ -7,6 +9,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+//First Solution
+function capitalize(str) {
+  const words = [];
+  for (let word of str.split(" ")) {
+    // from index 1 to last val return unchanged
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  // join the words
+  return words.join(" ");
+}
 
 module.exports = capitalize;
