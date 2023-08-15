7f6b25813d060bfa4f7dfbdb936b6009ce3b3fa0
Nithin Samuel <nithin.thewebdev@gmail.com>
2023-08-15 19:38:29 +0530

    added pyramid code

--- Grider/exercises/pyramid/index.js
+++ Grider/exercises/pyramid/index.js
@@ -9,11 +9,26 @@
 //   pyramid(2)
 //       ' # '
 //       '###'
-//   pyramid(3)
+//   pyramid(3)w
 //       '  #  '
 //       ' ### '
 //       '#####'
-
-function pyramid(n) {}
+//========================================//
+//to run >jest pyramid/test.js --watch
+function pyramid(n) {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let level = "";
+    // iterate over all the columns
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
